b620dad1cb0e0a7de1e0aaf4de52dab95990aff3
Borton Fweems <borton.fweems@browncloud.org>
2021-10-11 18:30:05 -0700

    make recursive drawing (a) not re-calc cumumats; and also (b) ...
    ... respect the new calc-'em-even-if-we're-not-drawing flag

--- src/p5zeugma/P5ZLimnable.java
+++ src/p5zeugma/P5ZLimnable.java
@@ -33,26 +33,34 @@ public interface P5ZLimnable  extends Limnable, IContainMultitudes
 
   default void RecursivelyDraw (PGraphicsOpenGL g, long ratch, Limnable.CumuMats cm)
     { boolean dr = QueryShouldDraw ();
-      boolean cdr = QueryShouldDrawChildrenEvenIfNotSelf ();
+      boolean drc = QueryShouldDrawChildrenEvenIfNotSelf ();
+      boolean ccm = QueryShouldCalcCumuMatsEvenIfNotDrawing ();
 
-      if (!dr  &&  ! cdr)
+      if (! dr  &&  ! drc  &&  ! ccm)
         return;
       
       boolean drf = QueryShouldDrawBeforeChildren ();
       
-      Limnable.CumuMats pushed_cm = cm;
-      cm = DependCumuMatsFrom (cm);
+      Limnable.CumuMats cur_cm = CurrentCumuMats ();
+      if (cur_cm != null
+    	  &&  (ratch <= cur_cm.rat_fresh  &&  ratch >= 0))
+        { }   // i.e. only don't if it's already freshly done
+      else
+    	{ cur_cm = DependCumuMatsFrom (cm);
+    	  if (ratch >= 0)
+            cur_cm.rat_fresh = ratch;
+    	}
 
       BefoDraw (g);
       
       if (dr  &&  drf)
         DrawSelf (g);
       
-      if (dr  ||  cdr)
+      if (dr  ||  drc)  // note the lack of " || ccm "...
         if (NumChildren ()  >  0)
           for (Zeubject z  :  Children ())
             if (z instanceof P5ZLimnable)
-              ((P5ZLimnable)z) . RecursivelyDraw (g, ratch, cm);
+              ((P5ZLimnable)z) . RecursivelyDraw (g, ratch, cur_cm);
       
       if (dr  &&  ! drf)
         DrawSelf (g);
